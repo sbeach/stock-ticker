2a7bbd9a34afc799685e4e7b5d3f4894ed63c978
Sean Beach <dirtybeach0728@gmail.com>
2015-05-31 17:02:29 -0500

    Implement Settings menu

--- app/src/main/java/com/seandbeach/stockticker/MainActivity.java
+++ app/src/main/java/com/seandbeach/stockticker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.seandbeach.stockticker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -26,13 +27,12 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
