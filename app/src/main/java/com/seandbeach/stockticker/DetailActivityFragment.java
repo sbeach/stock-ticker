5d3ab1193b15230c48d5edd805fd6a0199d09b16
Sean Beach <dirtybeach0728@gmail.com>
2015-06-02 02:12:45 -0500

    AppCompatActivity requires other Support library classes

--- app/src/main/java/com/seandbeach/stockticker/DetailActivityFragment.java
+++ app/src/main/java/com/seandbeach/stockticker/DetailActivityFragment.java
@@ -1,8 +1,12 @@
 package com.seandbeach.stockticker;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
@@ -14,13 +18,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.seandbeach.stockticker.data.StockContract;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DetailActivityFragment extends Fragment {
+public class DetailActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final int DETAIL_LOADER = 0;
     private String mQuoteStr;
+    private ShareActionProvider mShareActionProvider;
 
     public DetailActivityFragment() {
         setHasOptionsMenu(true);
@@ -31,8 +39,8 @@ public class DetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         Intent intent = getActivity().getIntent();
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            mQuoteStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (intent != null) {
+            mQuoteStr = intent.getDataString();
         }
 
         if (null != mQuoteStr) {
@@ -50,11 +58,11 @@ public class DetailActivityFragment extends Fragment {
         MenuItem menuItem = menu.findItem(R.id.action_share);
 
         // Get the provider and hold onto it to set/change the share intent.
-        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
         // Attach an intent to this ShareActionProvider.  You can update this at any time,
         // like when the user selects a new piece of data they might like to share.
-        if (mShareActionProvider != null ) {
+        if (mQuoteStr != null) {
             mShareActionProvider.setShareIntent(createShareQuoteIntent());
         } else {
             Log.d(LOG_TAG, "Share Action Provider is null?");
@@ -68,4 +76,51 @@ public class DetailActivityFragment extends Fragment {
         shareIntent.putExtra(Intent.EXTRA_TEXT, mQuoteStr);
         return shareIntent;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(DETAIL_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        Intent intent = getActivity().getIntent();
+        if (intent == null) {
+            return null;
+        }
+
+        return new CursorLoader(getActivity(),
+                intent.getData(),
+                StockQuoteFragment.STOCK_COLUMNS,
+                intent.getData().getQuery(),
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        if (!cursor.moveToFirst()) { return; }
+
+        String name = cursor.getString(StockQuoteFragment.COL_STOCK_NAME);
+
+        mQuoteStr = cursor.getString(StockQuoteFragment.COL_STOCK_SYMBOL)
+                + (name != null && !name.isEmpty() && !name.equals("null") ? (" (" + name + "): ") : ": ")
+                + cursor.getDouble(StockQuoteFragment.COL_STOCK_PRICE)
+                + " ("
+                    + cursor.getDouble(StockQuoteFragment.COL_STOCK_CHANGE)
+                    + ", " + cursor.getDouble(StockQuoteFragment.COL_STOCK_CHANGE_PERCENT) + "%"
+                + ")";
+
+        TextView detailTextView = (TextView)getView().findViewById(R.id.detail_text);
+        detailTextView.setText(mQuoteStr);
+
+        // If onCreateOptionsMenu has already happened, we need to update the share intent now.
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareQuoteIntent());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) { }
 }
